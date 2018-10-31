36edfade374888ce046fc455a261aa65b4deb113
Mark Drake <mdd@appdev4db.com>
2018-10-31 09:31:42 -0700

    Add MySQL back

---
+++ MySQL/SQL/JSON_IMPORT.sql
@@ -0,0 +1,438 @@
+/*
+**
+** MySQL JSON_IMPORT Function.
+**
+*/
+SET SESSION SQL_MODE=ANSI_QUOTES;
+--
+DROP FUNCTION IF EXISTS MAP_FOREIGN_DATATYPE;
+--
+DELIMITER $$
+--
+CREATE FUNCTION MAP_FOREIGN_DATATYPE(P_DATA_TYPE VARCHAR(128), P_DATA_TYPE_LENGTH INT, P_DATA_TYPE_SIZE INT) 
+RETURNS VARCHAR(128) DETERMINISTIC
+BEGIN
+
+  case P_DATA_TYPE
+    -- TODO : Enable Roundtrip for ENUM and SET
+	when 'set' 
+	  then return 'text';
+	when 'enum' 
+	  then return 'text';
+    -- Oracle Mappings
+	when 'VARCHAR2' 
+	  then return 'varchar';
+	when 'NUMBER'
+      then return 'decimal';
+	when 'CLOB'
+      then return 'text';
+	when 'NCLOB'
+      then return 'text';
+	when 'BFILE'
+      then return 'varchar(2048)';
+	when 'ROWID'
+      then return 'varchar(32)';
+    when 'XMLTYPE'
+      then return 'longtext';
+    when 'RAW'
+      then return 'varbinary';
+    when 'NVARCHAR2'
+      then return 'varchar';
+    when 'ANYDATA'
+      then return 'longtext';
+    -- SQLServer Mapppings
+	when 'nchar'
+      then return 'char';
+	when 'tinyint'
+      then return 'tinyint unsigned';
+	when 'bit'
+      then return 'tinyint(1)';
+	when 'real'
+      then return 'float';
+	when 'numeric'
+      then return 'decimal';
+	when 'money'
+      then return 'decimal';
+	when 'smallmoney'
+      then return 'decimal';
+	when 'char'
+      then case 
+             when P_DATA_TYPE_LENGTH = -1 then return 'longtext';
+             when P_DATA_TYPE_LENGTH > 16777215 then return 'longtext';
+             when P_DATA_TYPE_LENGTH > 65535  then return 'mediumtext';
+             when P_DATA_TYPE_LENGTH > 255  then return 'text';
+             else return 'char';
+           end case;
+	when 'nchar'
+      then case 
+             when P_DATA_TYPE_LENGTH = -1 then return 'longtext';
+             when P_DATA_TYPE_LENGTH > 16777215  then return 'longtext';
+             when P_DATA_TYPE_LENGTH > 65535  then return 'mediumtext';
+             when P_DATA_TYPE_LENGTH > 255  then return 'text';
+             else return 'char';
+           end case;
+	when 'nvarchar'
+      then case
+             when P_DATA_TYPE_LENGTH = -1 then return 'longtext';
+             when P_DATA_TYPE_LENGTH > 16777215  then return 'longtext';
+             when P_DATA_TYPE_LENGTH > 65535  then return 'mediumtext';
+             else return 'varchar';
+           end case;
+	when 'varchar'
+      then case
+             when P_DATA_TYPE_LENGTH = -1 then return 'longtext';
+             when P_DATA_TYPE_LENGTH > 16777215  then return 'longtext';
+             when P_DATA_TYPE_LENGTH > 65535  then return 'mediumtext';
+             else return 'varchar';
+           end case;
+	when 'datetime2'
+      then return 'datatime';
+	when 'smalldate'
+      then return 'datatime';
+	when 'datetimeoffset'
+      then return 'datatime';
+	when 'rowversion'
+      then return 'datatime';
+	when 'binary'
+      then case 
+             when P_DATA_TYPE_LENGTH > 16777215 then return 'longblob';
+             when P_DATA_TYPE_LENGTH > 65535  then return 'mediumblob';
+             when P_DATA_TYPE_LENGTH > 255  then return 'blob';
+             else return 'tinyblob';
+           end case;
+	when 'varbinary'
+      then case
+             when P_DATA_TYPE_LENGTH = -1 then return 'longblob';
+             when P_DATA_TYPE_LENGTH > 16777215  then return 'longblob';
+             when P_DATA_TYPE_LENGTH > 65535  then return 'mediumblob';
+             else return 'varbinary';
+           end case;
+	when 'text'
+      then case 
+             when P_DATA_TYPE_LENGTH = -1 then return 'longtext';
+             when P_DATA_TYPE_LENGTH > 16777215 then return 'longtext';
+             when P_DATA_TYPE_LENGTH > 65535  then return 'mediumtext';
+             when P_DATA_TYPE_LENGTH > 255  then return 'text';
+             else return 'char';
+           end case;
+	when 'ntext'
+      then case 
+             when P_DATA_TYPE_LENGTH = -1 then return 'longtext';
+             when P_DATA_TYPE_LENGTH > 16777215 then return 'longtext';
+             when P_DATA_TYPE_LENGTH > 65535  then return 'mediumtext';
+             when P_DATA_TYPE_LENGTH > 255  then return 'text';
+             else return 'char';
+           end case;
+	when 'image'
+      then case 
+             when P_DATA_TYPE_LENGTH = -1 then return 'longblob';
+             when P_DATA_TYPE_LENGTH > 16777215 then return 'longblob';
+             when P_DATA_TYPE_LENGTH > 65535  then return 'mediumblob';
+             when P_DATA_TYPE_LENGTH > 255  then return 'blob';
+             else return 'tinyblob';
+           end case;
+	when 'uniqueidentifier'
+      then return 'varchar(64)';
+	when 'hierarchyid'
+      then return 'varbinary(446)';
+	when 'xml'
+      then return 'longtext';
+	when 'geography'
+      then return 'json';
+	else
+   	  if (instr(P_DATA_TYPE,'TIME ZONE') > 0) then
+	    return 'timestamp';	
+      end if;
+	  if (INSTR(P_DATA_TYPE,'"."') > 0) then 
+	    return 'text';
+	  end if;
+      
+   	  if ((instr(P_DATA_TYPE,'INTERVAL') = 1)) then
+	    return 'varchar(16)';
+      end if;
+	  return lower(P_DATA_TYPE);
+  end case;
+end;
+$$
+--
+DELIMITER ;
+--
+DELIMITER $$
+--
+DROP PROCEDURE IF EXISTS GENERATE_SQL;
+--
+CREATE PROCEDURE GENERATE_SQL(P_TARGET_DATABASE VARCHAR(128), P_TABLE_NAME VARCHAR(128), P_COLUMN_LIST TEXT, P_DATA_TYPE_LIST TEXT, P_SIZE_CONSTRAINTS TEXT, INOUT P_DDL_STATEMENT TEXT, INOUT P_DML_STATEMENT TEXT) 
+BEGIN
+  DECLARE V_COLUMNS_CLAUSE     TEXT;
+  DECLARE V_INSERT_SELECT_LIST TEXT;
+  DECLARE V_COLUMN_PATTERNS    TEXT;
+  
+  with 
+    "SOURCE_TABLE_DEFINITIONS" 
+    as ( 
+      select c."KEY" IDX
+            ,c.VALUE "COLUMN_NAME"
+            ,t.VALUE "DATA_TYPE"
+            ,case
+               when s.VALUE = ''
+                 then NULL
+               when INSTR(s.VALUE,',') > 0
+                 then SUBSTR(s.VALUE,1,INSTR(s.VALUE,',')-1)
+               else
+                 s.VALUE
+             end "DATA_TYPE_LENGTH"
+            ,case
+               when INSTR(s.VALUE,',') > 0
+                 then SUBSTR(s.VALUE, INSTR(s.VALUE,',')+1)
+               else
+                 NULL
+             end "DATA_TYPE_SCALE"
+           from JSON_TABLE(CONCAT('[',P_COLUMN_LIST,']'),'$[*]' COLUMNS ("KEY" FOR ORDINALITY, "VALUE" VARCHAR(128) PATH '$')) c
+               ,JSON_TABLE(CONCAT('[',REPLACE(P_DATA_TYPE_LIST,'"."','\\".\\"'),']'),'$[*]' COLUMNS ("KEY" FOR ORDINALITY, "VALUE" VARCHAR(32) PATH '$')) t
+               ,JSON_TABLE(CONCAT('[',P_SIZE_CONSTRAINTS,']'),'$[*]' COLUMNS ("KEY" FOR ORDINALITY, "VALUE" VARCHAR(32) PATH '$')) s
+          where (c."KEY" = t."KEY") and (c."KEY" = s."KEY")
+    ),
+    "TARGET_TABLE_DEFINITIONS" 
+    as (
+      select std.*
+            , MAP_FOREIGN_DATATYPE("DATA_TYPE","DATA_TYPE_LENGTH","DATA_TYPE_SCALE") TARGET_DATA_TYPE
+        from "SOURCE_TABLE_DEFINITIONS" std
+    )
+    select group_concat(concat('"',COLUMN_NAME,'" ',TARGET_DATA_TYPE,
+                               case
+                                 when TARGET_DATA_TYPE like '%(%)'
+                                   then ''
+                                 when TARGET_DATA_TYPE like '%unsigned' 
+                                   then ''
+                                 when TARGET_DATA_TYPE in ('tinyint','smallint','mediumint','int','set','enum','tinytext','mediumtext','text','longtext','tinyblob','mediumblob','blob','longblob','json') 
+                                   then ''
+                                 when TARGET_DATA_TYPE in ('geometry','point','linestring','polygon','multipoint','multilinestring','multipolygon','geometrycollection')
+                                   then '' 
+                                 when DATA_TYPE in ('nchar','nvarchar')
+                                   then concat('(',DATA_TYPE_LENGTH,')',' CHARACTER SET UTF8MB4 ')
+                                 when DATA_TYPE_SCALE is not NULL
+                                   then concat('(',DATA_TYPE_LENGTH,',',DATA_TYPE_SCALE,')')
+                                 when DATA_TYPE_LENGTH is not NULL
+                                   then case when TARGET_DATA_TYPE in ('double')
+                                               then concat('(',DATA_TYPE_LENGTH,',0)')
+                                             else
+                                               concat('(',DATA_TYPE_LENGTH,')')
+                                             end
+                                 else
+                                   ''
+                               end,
+                               CHAR(32)
+                              )
+                     separator '  ,'
+                    ) COLUMNS_CLAUSE
+          ,group_concat(concat('"',COLUMN_NAME,'"') separator ',') INSERT_SELECT_LIST
+          ,group_concat(concat('"',COLUMN_NAME,'" ',
+                               TARGET_DATA_TYPE,
+                               case
+                                 when TARGET_DATA_TYPE like '%(%)'
+                                   then ''
+                                 when TARGET_DATA_TYPE like '%unsigned' 
+                                   then ''
+                                 when TARGET_DATA_TYPE in ('tinyint','smallint','mediumint','int','set','enum','tinytext','mediumtext','text','longtext','tinyblob','mediumblob','blob','longblob','json') 
+                                   then ''
+                                 when TARGET_DATA_TYPE in ('geometry','point','linestring','polygon','multipoint','multilinestring','multipolygon','geometrycollection')
+                                   then '' 
+                                 when DATA_TYPE_SCALE is not NULL
+                                   then concat('(',DATA_TYPE_LENGTH,',',DATA_TYPE_SCALE,')')
+                                 when DATA_TYPE_LENGTH is not NULL
+                                   then case when TARGET_DATA_TYPE in ('double')
+                                               then concat('(',DATA_TYPE_LENGTH,',0)')
+                                             else
+                                               concat('(',DATA_TYPE_LENGTH,')')
+                                        end
+                                 else
+                                   ''
+                               end,
+                               ' PATH ''$[',IDX-1,']''',CHAR(32)
+                              )
+                     separator '    ,'
+                    ) COLUMN_PATTERNS
+      into V_COLUMNS_CLAUSE, V_INSERT_SELECT_LIST, V_COLUMN_PATTERNS
+      from "TARGET_TABLE_DEFINITIONS";
+    SET P_DDL_STATEMENT = concat('create table if not exists "',P_TARGET_DATABASE,'"."',P_TABLE_NAME,'"(',CHAR(32),V_COLUMNS_CLAUSE,')'); 
+    SET P_DML_STATEMENT = concat('insert into "',P_TARGET_DATABASE,'"."',P_TABLE_NAME,'"(',P_COLUMN_LIST,')',CHAR(32),'select ',V_INSERT_SELECT_LIST,CHAR(32),'  from "JSON_STAGING",JSON_TABLE("DATA",''$.data."',P_TABLE_NAME,'"[*]'' COLUMNS (',CHAR(32),V_COLUMN_PATTERNS,')) data');
+END;
+$$
+--
+DELIMITER ;
+--
+DROP PROCEDURE IF EXISTS IMPORT_JSON;
+--
+DELIMITER $$
+--
+CREATE PROCEDURE IMPORT_JSON(P_TARGET_DATABASE VARCHAR(128), OUT P_RESULTS JSON) 
+BEGIN
+  DECLARE NO_MORE_ROWS      INT DEFAULT FALSE;
+  
+  DECLARE V_OWNER           VARCHAR(128);
+  DECLARE V_TABLE_NAME      VARCHAR(128);
+  DECLARE V_COLUMN_LIST     TEXT;
+  DECLARE V_DATA_TYPE_LIST  TEXT;
+  DECLARE V_SIZE_CONTRAINTS TEXT;
+
+  DECLARE V_STATEMENT     TEXT;
+  DECLARE V_DDL_STATEMENT TEXT;
+  DECLARE V_DML_STATEMENT TEXT;
+  
+  DECLARE V_START_TIME    BIGINT;
+  DECLARE V_END_TIME      BIGINT;
+  DECLARE V_ELAPSED_TIME  BIGINT;
+  
+  DECLARE V_ROW_COUNT     BIGINT;
+  
+  DECLARE V_SQLSTATE      INT;
+  DECLARE V_SQLERRM       TEXT;
+  
+  DECLARE TABLE_METADATA 
+  CURSOR FOR 
+  select OWNER
+        ,TABLE_NAME
+        ,COLUMN_LIST
+        ,DATA_TYPE_LIST
+        ,SIZE_CONSTRAINTS
+    from JSON_STAGING,
+	     JSON_TABLE(
+	       DATA,
+			'$.metadata.*' 
+		 	COLUMNS (
+			  OWNER                        VARCHAR(128) PATH '$.owner'
+			 ,TABLE_NAME                   VARCHAR(128) PATH '$.tableName'
+			 ,COLUMN_LIST                          TEXT PATH '$.columns'
+			 ,DATA_TYPE_LIST                       TEXT PATH '$.dataTypes'
+			 ,SIZE_CONSTRAINTS                     TEXT PATH '$.dataTypeSizing'
+			 ,INSERT_SELECT_LIST                   TEXT PATH '$.insertSelectList'
+             ,COLUMN_PATTERNS                      TEXT PATH '$.columnPatterns'
+		  )) c;
+
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET NO_MORE_ROWS = TRUE;
+  DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+  BEGIN 
+    GET DIAGNOSTICS CONDITION 1
+        V_SQLSTATE = RETURNED_SQLSTATE, V_SQLERRM = MESSAGE_TEXT;
+	SET P_RESULTS = JSON_ARRAY_APPEND(P_RESULTS,'$',JSON_OBJECT('error',JSON_OBJECT('severity','FATAL','tableName', V_TABLE_NAME,'sqlStatement', V_STATEMENT, 'code', V_SQLSTATE, 'msg', V_SQLERRM, 'details', 'unavailable' )));
+  END;  
+
+  SET SESSION SQL_MODE=ANSI_QUOTES;
+  SET SESSION group_concat_max_len = 131072;
+  
+  SET P_RESULTS = '[]';
+  SET NO_MORE_ROWS = FALSE;
+  OPEN TABLE_METADATA;
+  	
+  PROCESS_TABLE : LOOP
+	FETCH TABLE_METADATA INTO V_OWNER, V_TABLE_NAME, V_COLUMN_LIST, V_DATA_TYPE_LIST, V_SIZE_CONTRAINTS;
+    IF NO_MORE_ROWS THEN
+      LEAVE PROCESS_TABLE;
+    END IF;
+   
+    CALL GENERATE_SQL(P_TARGET_DATABASE, V_TABLE_NAME, V_COLUMN_LIST, V_DATA_TYPE_LIST, V_SIZE_CONTRAINTS, V_DDL_STATEMENT, V_DML_STATEMENT);
+   	-- SET P_RESULTS = JSON_ARRAY_APPEND(P_RESULTS,'$',JSON_OBJECT('info',JSON_OBJECT('DDL',V_DDL_STATEMENT)));
+   	-- SET P_RESULTS = JSON_ARRAY_APPEND(P_RESULTS,'$',JSON_OBJECT('info',JSON_OBJECT('DML',V_DML_STATEMENT)));
+    
+    SET V_STATEMENT = V_DDL_STATEMENT;
+    SET @STATEMENT = V_STATEMENT;
+    PREPARE STATEMENT FROM @STATEMENT;
+    EXECUTE STATEMENT;
+    DEALLOCATE PREPARE STATEMENT;
+	SET P_RESULTS = JSON_ARRAY_APPEND(P_RESULTS,'$',JSON_OBJECT('ddl',JSON_OBJECT('tableName', V_TABLE_NAME,'sqlStatement', V_DDL_STATEMENT)));
+    
+    SET V_STATEMENT = V_DML_STATEMENT;
+    SET @STATEMENT = V_STATEMENT;
+    PREPARE STATEMENT FROM @STATEMENT;
+	SET V_START_TIME = floor(unix_timestamp(current_timestamp(3)) * 1000);
+    EXECUTE STATEMENT;
+	SET V_ROW_COUNT = ROW_COUNT();
+	SET V_END_TIME =floor(unix_timestamp(current_timestamp(3)) * 1000);
+    DEALLOCATE PREPARE STATEMENT;
+   
+	SET V_ELAPSED_TIME = V_END_TIME - V_START_TIME;
+	SET P_RESULTS = JSON_ARRAY_APPEND(P_RESULTS,'$',JSON_OBJECT('dml',JSON_OBJECT('tableName', V_TABLE_NAME, 'rowCount', V_ROW_COUNT, 'elapsedTime',V_ELAPSED_TIME, 'sqlStatement', V_DML_STATEMENT)));
+  END LOOP;
+ 
+  CLOSE TABLE_METADATA;
+end;
+$$
+--
+DELIMITER ;
+--
+DROP FUNCTION IF EXISTS GENERATE_STATEMENTS;
+--
+DROP PROCEDURE IF EXISTS GENERATE_STATEMENTS;
+--
+DELIMITER $$
+--
+CREATE PROCEDURE GENERATE_STATEMENTS(P_TARGET_DATABASE VARCHAR(128), P_METADATA JSON, OUT P_RESULTS JSON)
+BEGIN
+  DECLARE NO_MORE_ROWS      INT DEFAULT FALSE;
+  
+  DECLARE V_OWNER           VARCHAR(128);
+  DECLARE V_TABLE_NAME      VARCHAR(128);
+  DECLARE V_COLUMN_LIST     TEXT;
+  DECLARE V_DATA_TYPE_LIST  TEXT;
+  DECLARE V_SIZE_CONTRAINTS TEXT;
+
+  DECLARE V_STATEMENT     TEXT;
+  DECLARE V_DDL_STATEMENT TEXT;
+  DECLARE V_DML_STATEMENT TEXT;
+  
+  
+  DECLARE V_SQLSTATE      INT;
+  DECLARE V_SQLERRM       TEXT;
+
+  DECLARE TABLE_METADATA 
+  CURSOR FOR 
+  select OWNER
+        ,TABLE_NAME
+        ,COLUMN_LIST
+        ,DATA_TYPE_LIST
+        ,SIZE_CONSTRAINTS
+    from JSON_TABLE(
+	      P_METADATA,
+			'$.metadata.*' 
+		 	COLUMNS (
+			  OWNER                        VARCHAR(128) PATH '$.owner'
+			 ,TABLE_NAME                   VARCHAR(128) PATH '$.tableName'
+			 ,COLUMN_LIST                          TEXT PATH '$.columns'
+			 ,DATA_TYPE_LIST                       TEXT PATH '$.dataTypes'
+			 ,SIZE_CONSTRAINTS                     TEXT PATH '$.dataTypeSizing'
+			 ,INSERT_SELECT_LIST                   TEXT PATH '$.insertSelectList'
+             ,COLUMN_PATTERNS                      TEXT PATH '$.columnPatterns'
+		  )) c;
+
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET NO_MORE_ROWS = TRUE;
+  DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+  BEGIN 
+    GET DIAGNOSTICS CONDITION 1
+        V_SQLSTATE = RETURNED_SQLSTATE, V_SQLERRM = MESSAGE_TEXT;
+	SET P_RESULTS = JSON_OBJECT('error',JSON_OBJECT('severity','FATAL','tableName', V_TABLE_NAME,'sqlStatement', V_STATEMENT, 'code', V_SQLSTATE, 'msg', V_SQLERRM, 'details', 'unavailable' ));
+  END;  
+
+  SET SESSION SQL_MODE=ANSI_QUOTES;
+  SET SESSION group_concat_max_len = 131072;
+  SET P_RESULTS = '{}';
+ 
+  SET NO_MORE_ROWS = FALSE;
+  OPEN TABLE_METADATA;
+  	
+  PROCESS_TABLE : LOOP
+	FETCH TABLE_METADATA INTO V_OWNER, V_TABLE_NAME, V_COLUMN_LIST, V_DATA_TYPE_LIST, V_SIZE_CONTRAINTS;
+    IF NO_MORE_ROWS THEN
+      LEAVE PROCESS_TABLE;
+    END IF;
+    
+    CALL GENERATE_SQL(P_TARGET_DATABASE, V_TABLE_NAME, V_COLUMN_LIST, V_DATA_TYPE_LIST, V_SIZE_CONTRAINTS, V_DDL_STATEMENT, V_DML_STATEMENT);
+    SET P_RESULTS = JSON_INSERT(P_RESULTS,concat('$."',V_TABLE_NAME,'"'),JSON_OBJECT('ddl', V_DDL_STATEMENT,'dml',V_DML_STATEMENT));
+     
+  END LOOP;
+ 
+  CLOSE TABLE_METADATA;
+end;
+$$
+--
+DELIMITER ;
+--
---
+++ MySQL/node/jTableImport.js
@@ -0,0 +1,197 @@
+"use strict";
+ 
+const fs = require('fs');
+const common = require('./common.js');
+const mysql = require('mysql');
+
+function connect(conn) {
+	
+  return new Promise(function(resolve,reject) {
+	                   conn.connect(function(err) {
+		                              if (err) {
+		                                reject(err);
+	                                  }
+  			                          resolve();
+                                    })
+				    })
+}	
+	  
+function query(conn,sqlQuery,args) {
+	
+  return new Promise(function(resolve,reject) {
+	                   conn.query(sqlQuery,args,function(err,rows,fields) {
+		                                     if (err) {
+		                                       reject(err);
+	                                         }
+											 resolve(rows);
+                                           })
+                     })
+}  
+	 
+async function createStagingTable(conn) {    	
+	const sqlStatement = `CREATE TEMPORARY TABLE IF NOT EXISTS "JSON_STAGING"("DATA" JSON)`;					   
+	const results = await query(conn,sqlStatement);
+	return results;
+}
+
+async function loadStagingTable(conn,dumpfilePath) {    	
+	const sqlStatement = `LOAD DATA LOCAL INFILE '${dumpfilePath}' INTO TABLE "JSON_STAGING" FIELDS ESCAPED BY ''`;					   
+	const results = await query(conn,sqlStatement);
+	return results;
+}
+
+async function verifyDataLoad(conn) {    	
+	const sqlStatement = `SELECT COUNT(*) FROM "JSON_STAGING"`;				
+	const results = await query(conn,sqlStatement);
+	return results;
+}
+
+async function processStagingTable(conn,schema) {    	
+	const sqlStatement = `SET @RESULTS = ''; CALL IMPORT_JSON(?,@RESULTS); SELECT @RESULTS "logRecords";`;					   
+	const results = await query(conn,sqlStatement,schema);
+	return results;
+}
+
+async function createTargetDatabase(conn,schema) {    	
+	const sqlStatement = `CREATE DATABASE IF NOT EXISTS "${schema}"`;					   
+	const results = await query(conn,sqlStatement,schema);
+	return results;
+}
+
+async function main(){
+	
+  let conn;
+  let parameters;
+  let sqlTrace;
+  let logWriter = process.stdout;
+    
+  let errorRaised = false;
+  let warningRaised = false;
+  let statusMsg = 'successfully';
+  
+  try {
+
+    parameters = common.processArguments(process.argv,'export');
+
+	if (parameters.LOGFILE) {
+	  logWriter = fs.createWriteStream(parameters.LOGFILE);
+    }
+
+	if (parameters.SQLTRACE) {
+	  sqlTrace = fs.createWriteStream(parameters.SQLTRACE);
+    }
+	
+    const connectionDetails = {
+            host      : parameters.HOSTNAME
+           ,user      : parameters.USERNAME
+           ,password  : parameters.PASSWORD
+		   ,database  : parameters.DATABASE
+		   ,multipleStatements: true
+    }
+
+    conn = mysql.createConnection(connectionDetails);
+	await connect(conn);
+    await query(conn,'SET SESSION SQL_MODE=ANSI_QUOTES');
+    await query(conn,`SET GLOBAL local_infile = 'ON'`);
+    
+    ;
+	
+    const dumpFilePath = parameters.FILE;	
+	const stats = fs.statSync(dumpFilePath)
+    const fileSizeInBytes = stats.size
+	
+    const logDML         = (parameters.LOGLEVEL && (parameters.loglevel > 0));
+    const logDDL         = (parameters.LOGLEVEL && (parameters.loglevel > 1));
+    const logDDLIssues   = (parameters.LOGLEVEL && (parameters.loglevel > 2));
+    const logTrace       = (parameters.LOGLEVEL && (parameters.loglevel > 3));
+    
+	let results = null;
+    const schema = parameters.TOUSER;
+	results = await createTargetDatabase(conn,schema);
+	results = await createStagingTable(conn);
+	const startTime = new Date().getTime();
+	results = await loadStagingTable(conn,dumpFilePath);
+	const elapsedTime = new Date().getTime() - startTime;
+    logWriter.write(`${new Date().toISOString()}: Import Data file "${dumpFilePath}". Size ${fileSizeInBytes}. Elapsed Time ${elapsedTime}ms.  Throughput ${Math.round((fileSizeInBytes/elapsedTime) * 1000)} bytes/s.\n`)
+
+	results = await processStagingTable(conn,schema);
+    results = results.pop();
+	results = JSON.parse(results[0].logRecords)
+
+    if ((parameters.DUMPLOG) && (parameters.DUMPLOG == 'TRUE')) {
+      const dumpFilePath = `${parameters.FILE.substring(0,parameters.FILE.lastIndexOf('.'))}.dump.import.${new Date().toISOString().replace(/:/g,'').replace(/-/g,'')}.json`;
+      fs.writeFileSync(dumpFilePath,JSON.stringify(results));
+    }
+       
+    results.forEach(function(result) {
+                      const logEntryType = Object.keys(result)[0];
+                      const logEntry = result[logEntryType];
+                      switch (true) {
+                        case (logEntryType === "dml") : 
+                             logWriter.write(`${new Date().toISOString()}: Table "${logEntry.tableName}". Rows ${logEntry.rowCount}. Elaspsed Time ${Math.round(logEntry.elapsedTime)}ms. Throughput ${Math.round((logEntry.rowCount/Math.round(logEntry.elapsedTime)) * 1000)} rows/s.\n`)
+                             break;
+                        case (logEntryType === "info") :
+                             logWriter.write(`${new Date().toISOString()}[INFO]: "${JSON.stringify(logEntry)}".\n`);
+                             break;
+                        case (logDML && (logEntryType === "dml")) :
+                             logWriter.write(`${new Date().toISOString()}: Table "${logEntry.tableName}".\n${logEntry.sqlStatement}".\n`)
+                             break;
+                        case (logDDL && (logEntryType === "ddl")) :
+                             logWriter.write(`${new Date().toISOString()}: Table "${logEntry.tableName}".\n${logEntry.sqlStatement}".\n`) 
+                             break;
+                        case (logTrace && (logEntryType === "trace")) :
+                             logWriter.write(`${new Date().toISOString()}: Table "${logEntry.tableName}".\n${logEntry.sqlStatement}".\n`)
+                             break;
+                        case (logEntryType === "error"):
+						     switch (true) {
+		                        case (logEntry.severity === 'FATAL') :
+                                  errorRaised = true;
+                                  logWriter.write(`${new Date().toISOString()}[${logEntry.severity}]: ${logEntry.tableName ? 'Table: "' + logEntry.tableName : ''}". Details:\n${logEntry.code} - ${logEntry.msg}\nSQL:${logEntry.sqlStatement}\n`)
+								  break
+								case (logEntry.severity === 'WARNING') :
+                                  warningRaised = true;
+                                  logWriter.write(`${new Date().toISOString()}[${logEntry.severity}]: ${logEntry.tableName ? 'Table: "' + logEntry.tableName : ''}". Details:\n${logEntry.code} - ${logEntry.msg}}\nSQL:${logEntry.sqlStatement}\n`)
+                                  break;
+                                case (logDDLIssues) :
+                                  logWriter.write(`${new Date().toISOString()}[${logEntry.severity}]: ${logEntry.tableName ? 'Table: "' + logEntry.tableName : ''}". Details:\n${logEntry.code} - ${logEntry.msg}\nSQL:${logEntry.sqlStatement}\n`)
+                             } 	
+                      }
+					  if ((parameters.SQLTRACE) && (logEntry.sqlStatement)) {
+						sqlTrace.write(`${logEntry.sqlStatement}\n\/\n`)
+					  }
+    })
+    
+	await conn.end();
+
+    statusMsg = warningRaised ? 'with warnings' : statusMsg;
+    statusMsg = errorRaised ? 'with errors'  : statusMsg;
+        
+    logWriter.write(`Import operation completed ${statusMsg}.`);
+    if (logWriter !== process.stdout) {
+      console.log(`Import operation completed ${statusMsg}: See "${parameters.LOGFILE}" for details.`);
+    }
+  } catch (e) {
+    if (logWriter !== process.stdout) {
+	  console.log(`Import operation failed: See "${parameters.LOGFILE}" for details.`);
+  	  logWriter.write('Import operation failed.\n');
+	  logWriter.write(e.stack);
+    }
+	else {
+    	console.log('Import operation Failed.');
+        console.log(e);
+	}
+    if (conn !== undefined) {
+	  await conn.end();
+	}
+  }
+  
+  if (logWriter !== process.stdout) {
+	logWriter.close();
+  }
+  
+  if (parameters.SQLTRACE) {
+    sqlTrace.close();
+  }
+}
+
+main();
