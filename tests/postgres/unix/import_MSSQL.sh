a42a0dedc76af8454014555493fdb10a4ba890fd
Mark Drake <mdd@appdev4db.com>
2019-03-30 22:50:39 +0000

    Postgres Linux Test Fixes
    Add database parameter to psql and node

--- tests/postgres/unix/import_MSSQL.sh
+++ tests/postgres/unix/import_MSSQL.sh
@@ -1,10 +1,10 @@
 export SRC=$1
 export SCHEMAVER=$2
 export FILEVER=$3
-node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD file=$SRC/Northwind$FILEVER.json        toUser=\"Northwind$SCHEMAVER\"      logFile=$IMPORTLOG mode=$MODE
-node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD file=$SRC/Sales$FILEVER.json            toUser=\"Sales$SCHEMAVER\"          logFile=$IMPORTLOG mode=$MODE
-node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD file=$SRC/Person$FILEVER.json           toUser=\"Person$SCHEMAVER\"         logFile=$IMPORTLOG mode=$MODE
-node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD file=$SRC/Production$FILEVER.json       toUser=\"Production$SCHEMAVER\"     logFile=$IMPORTLOG mode=$MODE
-node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD file=$SRC/Purchasing$FILEVER.json       toUser=\"Purchasing$SCHEMAVER\"     logFile=$IMPORTLOG mode=$MODE
-node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD file=$SRC/HumanResources$FILEVER.json   toUser=\"HumanResources$SCHEMAVER\" logFile=$IMPORTLOG mode=$MODE
-node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD file=$SRC/AdventureWorksDW$FILEVER.json toUser=\"DW$SCHEMAVER\"             logFile=$IMPORTLOG mode=$MODE
+node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD --database=$DB_DBNAME file=$SRC/Northwind$FILEVER.json        toUser=\"Northwind$SCHEMAVER\"      logFile=$IMPORTLOG mode=$MODE
+node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD --database=$DB_DBNAME file=$SRC/Sales$FILEVER.json            toUser=\"Sales$SCHEMAVER\"          logFile=$IMPORTLOG mode=$MODE
+node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD --database=$DB_DBNAME file=$SRC/Person$FILEVER.json           toUser=\"Person$SCHEMAVER\"         logFile=$IMPORTLOG mode=$MODE
+node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD --database=$DB_DBNAME file=$SRC/Production$FILEVER.json       toUser=\"Production$SCHEMAVER\"     logFile=$IMPORTLOG mode=$MODE
+node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD --database=$DB_DBNAME file=$SRC/Purchasing$FILEVER.json       toUser=\"Purchasing$SCHEMAVER\"     logFile=$IMPORTLOG mode=$MODE
+node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD --database=$DB_DBNAME file=$SRC/HumanResources$FILEVER.json   toUser=\"HumanResources$SCHEMAVER\" logFile=$IMPORTLOG mode=$MODE
+node $YADAMU_DB_ROOT/node/import --username=$DB_USER --hostname=$DB_HOST --password=$DB_PWD --database=$DB_DBNAME file=$SRC/AdventureWorksDW$FILEVER.json toUser=\"DW$SCHEMAVER\"             logFile=$IMPORTLOG mode=$MODE
