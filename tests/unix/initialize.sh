592251e3846c303d168c4a8f757af93ea1d273fb
Mark Drake <mdd@appdev4db.com>
2019-03-30 06:09:22 +0000

    ReCreate Work and JSON Folders.

--- tests/unix/initialize.sh
+++ tests/unix/initialize.sh
@@ -6,15 +6,16 @@ if [ -z ${YADAMU_HOME+x} ]; then export YADAMU_HOME=$(dirname "$(dirname "$YADAM
 YADAMU_DB=`basename $YADAMU_SCRIPT_ROOT`
 export YADAMU_DB_ROOT=$YADAMU_HOME/$YADAMU_DB
 export YADAMU_JSON_ROOT=$YADAMU_HOME/JSON
+if [ ! -e $YADAMU_JSON_ROOT ]; then mkidr -p $YADAMU_JSON_ROOT; fi
 export YADAMU_INPUT_PATH=$YADAMU_JSON_ROOT/$YADAMU_TARGET
 export YADAMU_WORK_ROOT=$YADAMU_HOME/work/$YADAMU_WORK_ID
-export | grep YADAMU
-if [ ! -e $YADAMU_WORK_ROOT ]; then mkdir $YADAMU_WORK_ROOT; fi
+if [ ! -e $YADAMU_WORK_ROOT ]; then mkdir -p $YADAMU_WORK_ROOT; fi
 export YADAMU_LOG_ROOT=$YADAMU_WORK_ROOT/logs
 . $YADAMU_HOME/tests/unix/initializeLogging.sh
 export YADAMU_OUTPUT_PATH=$YADAMU_WORK_ROOT/JSON
-if [ ! -e $YADAMU_OUTPUT_PATH ]; then . $YADAMU_HOME/tests/unix/createOutputFolders.:1,$ ::q $YADAMU_WORK_ROOT; fi
+if [ ! -e $YADAMU_OUTPUT_PATH ]; then . $YADAMU_HOME/tests/unix/createOutputFolders.sh $YADAMU_WORK_ROOT; fi
 export YADAMU_OUTPUT_PATH=$YADAMU_OUTPUT_PATH/$YADAMU_TARGET
 if [ -e $YADAMU_OUTPUT_PATH ]; then rm -rf $YADAMU_OUTPUT_PATH; fi
 mkdir -p $YADAMU_OUTPUT_PATH
+export | grep YADAMU
 . env/dbConnection.sh
---
+++ utilities/unix/installYadamu.sh
@@ -0,0 +1,8 @@
+rm master.zip
+rm -rf YADAMU---Yet-Another-DAta-Migration-Utility-master
+rm -rf yadamu
+curl -LOk https://github.com/markddrake/YADAMU---Yet-Another-DAta-Migration-Utility/archive/master.zip
+unzip master.zip
+mv YADAMU---Yet-Another-DAta-Migration-Utility-master yadamu
+cd yadamu
+mkdir work
