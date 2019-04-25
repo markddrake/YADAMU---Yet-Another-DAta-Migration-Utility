8a1f16818bab6a629a2443f41de88984a9605ca6
Mark Drake <mdd@appdev4db.com>
2019-04-25 09:09:50 -0800

    Revert "Refactor yadamuTest into testHarness"
    This reverts commit 259aa69f241c237df8f97edaff70aa2acbea1ce2.

--- tests/import/runTest.sh
+++ tests/import/runTest.sh
@@ -8,4 +8,4 @@ export YADAMU_LOG_ROOT=$YADAMU_WORK_ROOT/logs
 source $YADAMU_TEST_HOME/unix/initializeLogging.sh
 source $YADAMU_TEST_HOME/unix/installYadamu.sh
 source $YADAMU_TEST_HOME/unix/createDefaultUsers.sh
-node $YADAMU_TEST_HOME/node/testHarness CONFIG=$YADAMU_TEST_HOME/$YADAMU_WORK_ID/config.json >$YADAMU_LOG_PATH/$YADAMU_WORK_ID.log
+node $YADAMU_TEST_HOME/node/yadamuTest CONFIG=$YADAMU_TEST_HOME/$YADAMU_WORK_ID/config.json >$YADAMU_LOG_PATH/$YADAMU_WORK_ID.log
