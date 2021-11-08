e11894f41e15db3ee9f5193562b241750047220b
Mark Drake <markddrake@users.noreply.github.com>
2021-11-08 07:24:57 +0000

    QA Environment Docker configuration and script file updates

--- docker/windows/buildCommandLine.bat
+++ docker/windows/buildCommandLine.bat
@@ -1,5 +1,6 @@
 docker rmi -f yadamu/commandline:latest
-set DOCKER_BUILDKIT=1
-docker build -t  yadamu/commandline:latest . -f docker/dockerfiles/commandLine
-
-
+REM DOCKER_BUILDKIT is a Linux option only
+set DOCKER_BUILDKIT=0
+copy docker\dockerfiles\windows\commandline.dockerignore .dockerignore
+docker build -t  yadamu/commandline:latest . -f docker\dockerfiles\windows\commandLine
+del .dockerignore
