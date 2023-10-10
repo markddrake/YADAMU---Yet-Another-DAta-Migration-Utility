9ade145972e107aa223bc7e09964a63bcf5354ff
Mark Drake <markddrake@users.noreply.github.com>
2023-10-10 12:46:34 -0700

    QA Script updates for Oracle23 and Vertica 23

--- qa/setup/bin/exportUsers.sh
+++
@@ -1,3 +0,0 @@
-source $YADAMU_HOME/src/install/mssql/env/dbConnection.sh
-export MSSQL_SCHEMA=AdventureWorksAll
-sqlcmd -U$DB_USER -P$DB_PWD -S$DB_HOST -d$DB_DBNAME -I -e -i$YADAMU_QA_HOME/sql/mssql/RECREATE_SCHEMA.sql >> $YADAMU_LOG_PATH/mssql/RECREATE_SCHEMA.log
